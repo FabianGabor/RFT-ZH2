a2c2fd113502a6d5ebef75ef6f6fa5f7d16d29a6
Rzol3 <zolcsi00000001@gmail.com>
2020-12-01 09:40:54 +0100

    Kigenerált kód, use case és starUML

---
+++ Model1/Felhaasználó.java
@@ -0,0 +1,41 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Felhaasználó extends Lehetőség {
+
+    /**
+     * Default constructor
+     */
+    public Felhaasználó() {
+    }
+
+    /**
+     * 
+     */
+    public void Név;
+
+    /**
+     * 
+     */
+    public void Telefonszám;
+
+    /**
+     * 
+     */
+    public void Email;
+
+    /**
+     * 
+     */
+    public void Szül. dátum;
+
+    /**
+     * 
+     */
+    public void Anyja neve;
+
+
+}
